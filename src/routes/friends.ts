bfac72c1404040c2049dbf2f47394823dc7238da
jibongpark <bogjibong22@naver.com>
2025-06-26 22:28:47 +0900

    친구 api 스펙 변경, 리턴 형태 동기화
     - 호출 url 변경
     - 리턴시 res.ok, res.error로 정해진 패턴 따라가도록 변경

--- src/routes/friends.ts
+++ src/routes/friends.ts
@@ -9,68 +9,54 @@ export const friendRouter = express.Router();
 
 const domain = process.env.SERVER_DOMAIN
 
-friendRouter.post('/friend/createInvite', async (req, res) => {
-  try {
-    const userId = req.user?.id;
-    // 1시간 뒤 만료
-    const expiresAt = new Date(Date.now() + 60 * 60 * 1000);
-    // 랜덤 토큰 생성
-    const token = crypto.randomBytes(32).toString('hex');
-
-    const invite = await InviteModel.create({
-      inviter: userId,
-      token,
-      expiresAt,
-      used: false
-    });
-
-    // 초대 URL 생성
-    const inviteUrl = domain +`/invite/${token}`;
-
-    res.ok(201, "", { inviteUrl, expiresAt });
-  } catch (err) {
-    res.error(500, "Failed to create invite");
-  }
-});
-
-friendRouter.post('/friend/request/:token', async (req, res) => {
+friendRouter.post('/friend/request/:receiver', async (req, res) => {
     try {
-        const { token } = req.params;
-        const userId = req.user?.id; // 초대를 받는 사람
+        const { receiver } = req.params; // 신청을 받는 사람
+        const userId = req.user?.id; // 신청을 하는 사람
 
-        // 1. 토큰 검증
-        const invite = await InviteModel.findOne({ token });
-        if (!invite) {
-            res.status(400).json({ message: '존재하지 않거나 만료된 초대 코드입니다.' });
+        if (userId == receiver) {
+            res.error(500, '자기 자신은 추가할 수 없습니다.');
             return;
         }
 
-        if ( userId == invite.inviter.toString() ) {
-            res.error(500, "자기 자신은 추가할수 없습니다.");
+        const receiveUser = await UserModel.findById(receiver);
+        if (!receiveUser) {
+            res.error(400, '존재하지 않는 유저입니다.');
             return;
         }
 
         // 2. 이미 친구인지 확인
         const alreadyFriend = await FriendsModel.findOne({
-            userId: invite.inviter,
-            friendId: userId,
+            userId: userId,
+            friendId: receiver,
             status: { $in: ['pending', 'accepted'] }
         });
         if (alreadyFriend) {
-            res.status(409).json({ message: '이미 친구인 유저입니다.' });
+            res.error(409, '이미 친구인 유저입니다.');
             return;
         }
 
         // 3. 친구 요청 생성 (pending 상태)
         const friendship = await FriendsModel.create({
-            userId: invite.inviter,     // 초대한 사람
-            friendId: userId,           // 초대받은 사람
+            userId: userId,
+            friendId: receiver,
             status: 'pending'
         });
-        
-        await InviteModel.deleteOne({ token });
 
-        res.ok(201);
+        const senderUser = await UserModel.findById(userId);
+        if (!senderUser) {
+            res.error(500 ,'발신자 정보를 불러오는 데 실패했습니다.');
+            return;
+        }
+
+        const friendRequestDTO = {
+            senderId:   senderUser._id.toString(),
+            senderName: senderUser.name,
+            receiverId: receiveUser._id.toString(),
+            receiverName: receiveUser.name
+        };
+
+        res.ok(201, '친구 요청 완료', friendRequestDTO);
     } catch (err: any) {
         res.error(500);
     }
@@ -82,8 +68,8 @@ friendRouter.post('/friend/accept/:friendId', async (req, res) => {
         const { friendId } = req.params;
 
         const friendship = await FriendsModel.findOne({
-            userId: userId,
-            friendId: friendId,
+            userId: friendId, // 상대방이 나에게 요청을 했음으로 userId는 상대방 아이디
+            friendId: userId,
             status: 'pending'
         });
 
